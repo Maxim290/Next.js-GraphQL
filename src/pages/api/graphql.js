acede3fa53575fc820aaefaa1b3ab6fa1c235cd2
Maxim290 <maxim.mironovdev@gmail.com>
2025-03-14 16:02:22 +0100

    feat(api)

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,5 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ApolloServer } from "apollo-server-micro";
+import mongoose from "mongoose";
+import typeDefs from "../../graphql/typeDefs";
+import resolvers from "../../graphql/resolvers";
+import { verifyToken } from "../../utils/auth";
+import errorHandler from "../../utils/errorHandler";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!global.mongoose) {
+    global.mongoose = mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 }
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        const token = req.headers.authorization || "";
+        return { user: verifyToken(token) };
+    },
+    formatError: errorHandler,
+});
+
+export const config = { api: { bodyParser: false } };
+export default apolloServer.createHandler();
